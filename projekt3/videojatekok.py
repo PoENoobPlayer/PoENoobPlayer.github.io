cc39daf68c61fc14d0118ff4989ac771a0d8be23
Mate6666 <99018798+Mate6666@users.noreply.github.com>
2023-02-16 13:58:44 +0100

    Add files via upload

--- projekt3/videojatekok.py
+++ projekt3/videojatekok.py
@@ -1,4 +1,9 @@
 #1 Feladat: sorok megszámolása
 with open("videojatekok.txt","r", encoding="utf-8") as jatekok:
-    for sor in jatekok:
-        print(len(sor.split("-")))
+    jatekok = [j.strip().split("-") for j in jatekok]
+# print(jatekok)
+def jatekokszama(): 
+    jatek = 0
+    for jatek in jatekok:
+            jatek = jatek +1
+    print(f"A játékok száma:{jatek}")
