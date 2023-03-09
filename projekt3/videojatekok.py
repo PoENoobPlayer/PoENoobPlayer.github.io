f1c3d37046ee043d2d1da03242f9f5cf7c062460
unknown <tanulo@t10g46.hunyadi.local>
2023-03-09 13:53:34 +0100

    A játékok adatainak kiíratása

--- projekt3/videojatekok.py
+++ projekt3/videojatekok.py
@@ -1,9 +1,15 @@
-#1 Feladat: sorok megszámolása
-with open("videojatekok.txt","r", encoding="utf-8") as jatekok:
-    jatekok = [j.strip().split("-") for j in jatekok]
-# print(jatekok)
-def jatekokszama(): 
-    jatek = 0
-    for jatek in jatekok:
-            jatek = jatek +1
-    print(f"A játékok száma:{jatek}")
+class Jatekok():
+    def __init__(self,nev,ev,ar,platform):
+        self.nev = nev
+        self.ev = ev
+        self.ar = ar
+        self.platform = platform
+fajl = open("videojatekok.txt", "r", encoding="utf-8")
+
+videojatekok_listaja = []
+for sorok in fajl:
+    oszlop = sorok.strip().split("-")
+    videojatekok_listaja.append(Jatekok(oszlop[0], oszlop[1], oszlop[2], oszlop[3]))
+
+for item in videojatekok_listaja:
+    print(f"Játék neve: {item.nev}, Kiadási év: {item.ev}, Ára: {item.ar}, Platformokon elérhető: {item.platform}")
